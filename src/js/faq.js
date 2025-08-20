ca9e9c98976e9f4762755b773a24488f5e9bfb70
YevhenKharchenko <evgenvalerych@gmail.com>
2025-08-20 22:12:18 +0300

    Add faq section

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,4 +1,53 @@
-import sprite from '../img/sprite.svg';
+import Swiper from 'swiper';
+import 'swiper/css/bundle';
+
+const faqDots = document.querySelectorAll('.faq-dot');
+
+let faqSwiper;
+
+faqSwiper = new Swiper('.faq-swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  grabCursor: true,
+  slidesPerView: 1,
+  initialSlide: 0,
+  spaceBetween: 32,
+  grabCursor: true,
+  allowTouchMove: true,
+  speed: 500,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1440: {
+      slidesPerView: 5,
+      grabCursor: false,
+      allowTouchMove: false,
+      spaceBetween: 0,
+    },
+  },
+  on: {
+    init: () => {
+      document.querySelector('.faq-swiper-container').classList.add('show');
+    },
+    slideChange: function () {
+      updateFaqDots(this.realIndex);
+    },
+  },
+});
+
+function updateFaqDots(index) {
+  faqDots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
+}
+
+faqDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    faqSwiper.slideTo(index);
+  });
+});
 
 const faqBtns = document.querySelectorAll('.faq-top-text-btn');
 faqBtns.forEach(btn => btn.addEventListener('click', onBtnClick));
@@ -7,13 +56,11 @@ function onBtnClick(e) {
   const faqTop = e.currentTarget;
   const faqContainer = faqTop.closest('.faq-list-item');
   const bottomText = faqContainer.querySelector('.faq-bottom-text');
-  const iconUse = faqTop.querySelector('use');
 
   document.querySelectorAll('.faq-list-item').forEach(item => {
     if (item !== faqContainer) {
       item.classList.remove('faq-open');
       item.querySelector('.faq-bottom-text').classList.remove('is-visible');
-      item.querySelector('use').setAttribute('href', `${sprite}#icon-plus`);
     }
   });
 
@@ -21,9 +68,7 @@ function onBtnClick(e) {
 
   if (bottomText.classList.contains('is-visible')) {
     faqContainer.classList.add('faq-open');
-    iconUse.setAttribute('href', `${sprite}#icon-minus`);
   } else {
     faqContainer.classList.remove('faq-open');
-    iconUse.setAttribute('href', `${sprite}#icon-plus`);
   }
 }
